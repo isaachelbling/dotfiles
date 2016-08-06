b078837b9b280ac2f1c2a0257e2a6bf60413e5b5
Isaac Helbling <imhelbling@gmail.com>
2016-08-06 19:27:27 +0000

    customization

--- os/ubuntu/file_aliases.sh
+++ os/ubuntu/file_aliases.sh
@@ -1,3 +1,13 @@
 # ruby/rails
 alias be='bundle exec'
 alias bers='be rspec --drb'
+
+# general
+alias py='python3.5'
+alias pyserv='python2 -m SimpleHTTPServer'
+alias pyut='python3.5 -m unittest spec.py'
+alias pytest='py.test -v spec.py'
+alias th='thunar'
+
+# navigation
+alias cdpy='cd ~/coding/python'
